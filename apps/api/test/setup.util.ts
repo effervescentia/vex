226b71dd4146ed7851e9d6e46a925085e2d61a6e
Ben Teichman <ben@voiceflow.com>
2025-10-21 12:43:33 -0400

    fix: working tests

--- apps/api/test/setup.util.ts
+++ apps/api/test/setup.util.ts
@@ -1,4 +1,15 @@
+import type { Environment } from '@api/app/app.env';
 import * as schema from '@api/db/db.schema';
 import { integrationTestFactory } from '@bltx/test';
 
-export const setupIntegrationTest = integrationTestFactory(schema);
+export const setupIntegrationTest = integrationTestFactory(schema, {
+  PORT: 8080,
+
+  POSTGRES_DATABASE: 'test',
+  POSTGRES_USERNAME: 'test',
+  POSTGRES_PASSWORD: 'test',
+
+  ACCOUNT_MAX_ALIASES: 3,
+  ACCOUNT_ALIAS_EXPIRY_SHORT: 1000,
+  ACCOUNT_ALIAS_EXPIRY_LONG: 10000,
+} satisfies Environment);
