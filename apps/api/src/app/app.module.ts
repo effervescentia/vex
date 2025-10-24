6fcb21cf42d77481ab19d8d5db79948921b769bc
Ben Teichman <ben@voiceflow.com>
2025-10-24 15:28:06 -0400

    fix: remove logger plugin for now

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,17 +2,16 @@ import { AccountController } from '@api/account/account.controller';
 import { EnvironmentPlugin } from '@api/global/environment.plugin';
 import { HealthController } from '@api/global/health.controller';
 import { PostController } from '@api/post/post.controller';
-import { LoggerPlugin } from '@bltx/core';
 import { cors } from '@elysiajs/cors';
 import Elysia from 'elysia';
 
 export type App = typeof App;
 
+// TODO: write custom logger plugin
 export const App = new Elysia()
 
   .use(cors())
   .use(EnvironmentPlugin)
-  // .use(LoggerPlugin)
   .use(HealthController)
 
   .use(AccountController)
