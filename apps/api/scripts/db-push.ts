cf5f0d0b63ad9a507e6cf4fe3deaf8a9152e18bb
Ben Teichman <ben@voiceflow.com>
2025-10-29 03:25:51 -0400

    feat: run database migrations in kubernetes (ENG-68) (#5)

--- apps/api/scripts/db-push.ts
+++
@@ -1,5 +0,0 @@
-import { DatabasePlugin } from '@api/db/db.plugin';
-import * as schema from '@api/db/db.schema';
-import { dbPush } from '@bltx/db';
-
-await dbPush(DatabasePlugin.decorator.db, schema);
