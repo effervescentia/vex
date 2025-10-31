fe4a53a78a7675bb32f4191cf4d596ff989fef62
Ben Teichman <ben@voiceflow.com>
2025-10-31 01:14:49 -0400

    refactor: auth plugin

--- apps/api/src/post/post.controller.ts
+++ apps/api/src/post/post.controller.ts
@@ -1,9 +1,6 @@
 import { AuthPlugin } from '@api/auth/auth.plugin';
-import { AuthSessionService } from '@api/auth/auth-session.service';
-import { AuthSessionDB } from '@api/auth/data/auth-session.db';
 import { DatabasePlugin } from '@api/db/db.plugin';
 import { EnvironmentPlugin } from '@api/global/environment.plugin';
-import { eq } from 'drizzle-orm';
 import Elysia, { t } from 'elysia';
 import { CreateTextPostRequest } from './data/create-text-post.req';
 import { PatchTextPostRequest } from './data/patch-text-post.req';
@@ -21,34 +18,6 @@ export const PostController = new Elysia({ prefix: '/post' })
   .use(DatabasePlugin)
   .use(EnvironmentPlugin)
   .use(AuthPlugin)
-  .macro({
-    authenticated: {
-      resolve: async ({ db, env, cookie: { accessToken } }) => {
-        const sessionService = new AuthSessionService(db(), env());
-
-        try {
-          if (typeof accessToken.value !== 'string') throw new UnauthorizedError();
-
-          const tokenData = await sessionService.accessToken.verify(accessToken.value);
-          if (!tokenData) throw new UnauthorizedError();
-
-          const session = await db().query.AuthSessionDB.findFirst({
-            where: eq(AuthSessionDB.id, tokenData.sessionID),
-            with: { credential: { with: { account: true }, columns: {} } },
-            columns: {},
-          });
-          if (!session) throw new UnauthorizedError();
-
-          accessToken.value = await sessionService.accessToken.sign({ sessionID: tokenData.sessionID });
-
-          return { principal: session.credential.account };
-        } catch (err) {
-          accessToken?.remove();
-          throw err;
-        }
-      },
-    },
-  })
   .derive({ as: 'scoped' }, ({ db }) => ({ service: new PostService(db()) }))
 
   .get(
